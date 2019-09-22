8d404995ba429ebbf0a58f83ce19d23baa05639e
Sergey Mikhno <sergey.mikhno@gmail.com>
2019-09-22 11:24:13 +0200

    added about.html

--- routes.py
+++ routes.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/about")
+def index():
+    return render_template("about.html")
+
 @app.route("/")
 def index():
     return render_template("index.html")
